f2eedb014ada693d5b2f6897c1e78e7d4aa1c1ab
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2023-02-09 09:48:21 -0300

    Refactor 9.cpp

--- códigos/9.cpp
+++ códigos/9.cpp
@@ -2,13 +2,13 @@
 #include <iostream> // std::cout, std::fixed
 #include <iomanip>  // std::setprecision
 
-int main()
+int main(void)
 {
     long double f = 3.141593212L;
     std::cout << f << '\n';
 
-    std::streamsize ss = std::cout.precision();
-    std::cout << "Initial precision = " << ss << '\n';
+    const auto default_precision{std::cout.precision()};
+    std::cout << "Initial precision = " << default_precision << '\n';
 
     std::cout << std::setprecision(5) << f << '\n';
     std::cout << std::setprecision(9) << f << '\n';
@@ -20,13 +20,23 @@ int main()
     // so that you can better see the precision. It prevents the program from
     // switching to E notation for large values and causes the program to display six
     // digits to the right of the decimal
+    // alternative way (book CPlus Primer)
     // std::cout.setf(ios_base::fixed, ios_base::floatfield); // fixed-point
-    // std::cout.unsetf(std::ios_base::fixed);
+    // std::cout.unsetf(std::ios_base::floatfield); // go to default mode
 
     // https://cplusplus.com/reference/ios/fixed/
-    // The default notation can be selected by calling str.unsetf(ios_base::floatfield)
-    std::cout.unsetf(std::ios_base::floatfield); // go to default mode
-    // std::defaultfloat;
-    std::cout << std::setprecision(ss) << f << '\n';
+    std::cout << std::setprecision(default_precision); // go to default mode
+    std::cout << std::defaultfloat;                    // go to default mode
+    std::cout << f << std::endl;
     return 0;
 }
+
+// fixed: set precision two decimal places
+// const auto default_precision {std::cout.precision()};
+// cout << fixed << std::setprecision(2);
+// std::cout << std::setprecision(default_precision); // restore defaults
+// cout << std::defaultfloat; // restore defaults
+
+// fixed: alternative way (book CPlus Primer)
+// std::cout.setf(ios_base::fixed, ios_base::floatfield); // fixed-point
+// std::cout.unsetf(std::ios_base::floatfield); // go to default mode
