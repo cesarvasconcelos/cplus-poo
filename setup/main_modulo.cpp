8375f250991b0c902d5de70321ed35624212a78e
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2023-02-13 10:04:23 -0300

    Incluir exemplo módulos em C++

---
+++ setup/main_modulo.cpp
@@ -0,0 +1,9 @@
+// busca o ".hpp" no diretorio corrente
+// antes de buscar no diretório de headers do sistema
+#include "modulo.hpp"
+
+int main(void)
+{
+    funcao_util(55);
+    return 0;
+}
---
+++ setup/modulo.cpp
@@ -0,0 +1,10 @@
+// arquivo com protótipos + implementação
+#include "modulo.hpp"
+#include <iostream>
+
+using namespace std;
+
+void funcao_util(int a)
+{
+    cout << "a=" << a << "\n";
+}
